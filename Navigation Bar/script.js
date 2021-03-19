e7474f0ab85d3a6c7decbae5e79ee5edf80afdfc
batuhanmeric <Batuhann.mericc@gmail.com>
2021-03-19 23:16:25 +0300

    fixed

---
+++ Navigation Bar/script.js
@@ -0,0 +1,10 @@
+function myFunction(){
+    var x = document.getElementById("menu");
+
+    if(x.style.display == "block"){
+        x.style.display ="none";
+    }
+    else{
+        x.style.display ="block";
+    }
+}
