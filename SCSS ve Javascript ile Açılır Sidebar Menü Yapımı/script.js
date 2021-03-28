40306bd8ad75f62db10805a36cc0d88b40742268
batuhanmeric <Batuhann.mericc@gmail.com>
2021-03-28 03:46:23 +0300

    fixed

---
+++ SCSS ve Javascript ile Açılır Sidebar Menü Yapımı/script.js
@@ -0,0 +1,9 @@
+function menuAc(){
+    document.getElementById('side-menu').style.width = "250px";
+    document.getElementById('content').style.marginLeft = "250px";
+}
+
+function menuKapat(){
+    document.getElementById('side-menu').style.width = "0";
+    document.getElementById('content').style.marginLeft = "0";
+}
